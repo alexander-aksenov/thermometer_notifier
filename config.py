9192b0d943eaf4adb0f08e611ff9135b220efc5c
Alexander Aksenov <alexander.aksenov@mail.ru>
2023-03-18 18:01:46 +0300

    Add config

---
+++ config.py
@@ -0,0 +1,17 @@
+# Config for thermometer_notifier
+
+# Temperature zones - tuples where first number is lower bound, second - high bound
+GREEN_ZONE=(15, 19)
+YELLOW_ZONE=(12, 21)
+
+# GPIO where traffic lights connected
+RED_LIGHT_GPIO=22
+YELLOW_LIGHT_GPIO=23
+GREEN_LIGHT_GPIO=24
+
+# Thermometer data GPIO
+THERMOMETER_GPIO=4
+
+LOG_FILE="temp.log"
+
+ITER_DELAY_TIME=3
--- thermometer_notifier.py
+++ thermometer_notifier.py
@@ -6,58 +6,58 @@ from data_storage import DataStorage
 from thermometer import Thermometer
 from logger import Logger
 from tgbot import TgBot
+import config
 
 
 TOKEN = os.getenv('TOKEN')
 
-log_file = "temp.log"
-
-therm_gpio = 4
-
-red_gpio = 22
-green_gpio = 24
-yellow_gpio = 23
-
-red_threshold = 21
-yellow_threshold = 19
-
 def red_light_on(pi):
-    pi.write(red_gpio, pigpio.HIGH)
-    pi.write(yellow_gpio, pigpio.LOW)
-    pi.write(green_gpio, pigpio.LOW)
+    pi.write(config.RED_LIGHT_GPIO, pigpio.HIGH)
+    pi.write(config.YELLOW_LIGHT_GPIO, pigpio.LOW)
+    pi.write(config.GREEN_LIGHT_GPIO, pigpio.LOW)
 
 def yellow_light_on(pi):
-    pi.write(yellow_gpio, pigpio.HIGH)
-    pi.write(red_gpio, pigpio.LOW)
-    pi.write(green_gpio, pigpio.LOW)
+    pi.write(config.YELLOW_LIGHT_GPIO, pigpio.HIGH)
+    pi.write(config.RED_LIGHT_GPIO, pigpio.LOW)
+    pi.write(config.GREEN_LIGHT_GPIO, pigpio.LOW)
 
 def green_light_on(pi):
-    pi.write(green_gpio, pigpio.HIGH)
-    pi.write(yellow_gpio, pigpio.LOW)
-    pi.write(red_gpio, pigpio.LOW)
+    pi.write(config.GREEN_LIGHT_GPIO, pigpio.HIGH)
+    pi.write(config.YELLOW_LIGHT_GPIO, pigpio.LOW)
+    pi.write(config.RED_LIGHT_GPIO, pigpio.LOW)
 
 def all_lights_off(pi):
-    pi.write(green_gpio, pigpio.LOW)
-    pi.write(yellow_gpio, pigpio.LOW)
-    pi.write(red_gpio, pigpio.LOW)
+    pi.write(config.GREEN_LIGHT_GPIO, pigpio.LOW)
+    pi.write(config.YELLOW_LIGHT_GPIO, pigpio.LOW)
+    pi.write(config.RED_LIGHT_GPIO, pigpio.LOW)
+
+def check_temp_is_green(temp):
+    if temp >= config.GREEN_ZONE[0] and temp <= config.GREEN_ZONE[1]:
+        return True
+    return False
+
+def check_temp_is_yellow(temp):
+    if temp >= config.YELLOW_ZONE[0] and temp <= config.YELLOW_ZONE[1]:
+        return True
+    return False
 
 def data_read_iter(thermometer, logger, data_storage, pi, tgbot):
     thermometer.trigger()
-    time.sleep(3)
+    time.sleep(config.ITER_DELAY_TIME)
     logger.write()
     temp = data_storage.temperature
     temp_out = "%.1f" % data_storage.temperature
     hum_out = "%.1f" % data_storage.humidity
     time_out = data_storage.time
     print(str(time_out) + ": " + str(temp_out) + "° " + str(hum_out) + "%")
-    if temp >= red_threshold:
-        tgbot.send_temperature()
-        red_light_on(pi)
-    elif temp >= yellow_threshold:
+    if check_temp_is_green(temp):
+        green_light_on(pi)
+    elif check_temp_is_yellow(temp):
         tgbot.send_temperature()
         yellow_light_on(pi)
     else:
-        green_light_on(pi)
+        tgbot.send_temperature()
+        red_light_on(pi)
 
 def read_thread(thermometer, logger, data_storage, pi, tgbot):
     i = 0
@@ -69,14 +69,14 @@ def read_thread(thermometer, logger, data_storage, pi, tgbot):
 pi = pigpio.pi()
 if pi.connected:
     print('pi connected')
-    pi.set_mode(red_gpio, pigpio.INPUT)
-    pi.set_mode(yellow_gpio, pigpio.INPUT)
-    pi.set_mode(green_gpio, pigpio.INPUT)
+    pi.set_mode(config.RED_LIGHT_GPIO, pigpio.INPUT)
+    pi.set_mode(config.YELLOW_LIGHT_GPIO, pigpio.INPUT)
+    pi.set_mode(config.GREEN_LIGHT_GPIO, pigpio.INPUT)
 
     data_storage = DataStorage()
-    logger = Logger(log_file, data_storage)
+    logger = Logger(config.LOG_FILE, data_storage)
 
-    thermometer = Thermometer(therm_gpio, pi, data_storage)
+    thermometer = Thermometer(config.THERMOMETER_GPIO, pi, data_storage)
     thermometer.start()
 
     tgbot = TgBot.create(TOKEN, data_storage)
